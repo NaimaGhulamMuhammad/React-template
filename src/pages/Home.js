980f3977638eb857f9e1cb7e638cfd944e6852fe
Naima Ghulam Muhammad <63257312+NaimaGhulamMuhammad@users.noreply.github.com>
2021-03-15 19:44:16 +0500

    blog portfolio aboutus services

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,27 @@
 import Navbar from "../components/Navbar";
 import Topbar from "../components/Topbar";
-import Slider from "../sections/Slider"
-export default function Home() {
+import Slider from "../sections/Slider";
+import Ourteam from "../sections/Ourteam";
+import BlogPost from "../sections/BlogPost";
+import Services from "../sections/Services";
+import AboutUs from "../sections/AboutUs";
+import Qualities from "../sections/Qualities";
+import Portfolio from "../sections/Portfolio";
 
+export default function Home() {
   return (
-    <div>
-      <Topbar/>
-      <Navbar />
-      <Slider />
-    </div>
+    <>
+      <div className="wraper">
+        <Topbar />
+        <Navbar />
+        <Slider />
+        <Qualities />
+        <AboutUs />
+        <Services />
+        <Ourteam />
+        <BlogPost />
+        <Portfolio />
+      </div>
+    </>
   );
 }
